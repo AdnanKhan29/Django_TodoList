93ef81e242cc9b103e9a702152f33c55f0312500
AdnanKhan29 <102600483+AdnanKhan29@users.noreply.github.com>
2023-11-02 19:01:44 +0530

    finished

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
         views.update_task_status,
         name="update_status",
     ),
+     path("delete/<int:task_id>", views.delete_task, name="delete"),
+     path("filter/<str:status>", views.filter_tasks, name="filter")
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -35,3 +35,35 @@ def update_task_status(
     success_url = reverse("index")
  
     return HttpResponseRedirect(success_url)
+
+def delete_task(request: HttpRequest, task_id: int) -> HttpResponse:
+ 
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+ 
+    success_url = reverse("index")
+ 
+    return HttpResponseRedirect(success_url)
+
+def filter_tasks(request: HttpRequest, status: str) -> HttpResponse:
+ 
+    if not status in Task.StatusChoice.values:
+        return HttpResponseBadRequest("Invalid status")
+ 
+    if request.method == "POST":
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+ 
+    tasks = Task.objects.filter(status=status).order_by("-created")
+ 
+    form = TaskForm()
+ 
+    context = {
+        "tasks": tasks,
+        "form": form,
+        "Status": Task.StatusChoice,
+        "filter_choice": status,
+    }
+ 
+    return render(request, "index.html", context)
