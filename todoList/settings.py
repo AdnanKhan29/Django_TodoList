124aae4fe8528d3a20790152d3410178d100f22d
Adnan Khan <102600483+AdnanKhan29@users.noreply.github.com>
2023-11-02 19:11:55 +0530

    Update settings.py

--- todoList/settings.py
+++ todoList/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(*urlmzhr8jm=!pg3s6^gk6m&1ok434#ienc9v!ahnr8&heq!z'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
