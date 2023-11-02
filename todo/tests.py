51b4e0ea306c78eebf7a21b21fe49d08eaa056cf
AdnanKhan29 <102600483+AdnanKhan29@users.noreply.github.com>
2023-11-02 18:38:05 +0530

    added html and css files and connected to database

--- todo/tests.py
+++ todo/tests.py
@@ -1,38 +1,47 @@
+# todo/tests.py
+ 
 import datetime
-
-from django.test import TestCase
-
+ 
+from django.db.models import QuerySet
+from django.http import HttpResponse
+from django.urls import reverse
+from django.test import TestCase, Client
+ 
 from todo.models import Task
-
-
+ 
 class TestTaskModel(TestCase):
+     ...
+ 
+class TestIndexView(TestCase):
     @classmethod
     def setUpTestData(cls):
-        """Creates a task instance that our tests can access.
-        setUpTestData is executed once, so our task instance is
-        shared between tests.
-        """
-        cls.task_name = "Book dentist appointment"
-        cls.task = Task.objects.create(name=cls.task_name)
-
-    def test_task_name(self):
-        self.assertEqual(self.task.name, self.task_name)
-
-    def test_task_default_status(self):
-        """Test that our task was assigned a status of 'To Do'"""
-
-        expected = Task.StatusChoice.TODO
-        actual = self.task.status
-
-        self.assertEqual(expected, actual)
-
-    def test_task_created(self):
-        """Tests that task.created stores a datetime"""
-
-        self.assertEqual(type(self.task.created), datetime.datetime)
-
-    def test_str(self):
-        expected = f"To Do: {self.task_name}"
-        actual = str(self.task)
-
-        self.assertEqual(expected, actual)
+        cls.url = reverse("index")
+        cls.client = Client()
+        cls.task = Task.objects.create(name="book dentist appointment")
+ 
+    def test_index_view_returns_httpresponse(self):
+        """Test our view returns a HttpResponse"""
+ 
+        response = self.client.get(self.url)
+ 
+        self.assertTrue(isinstance(response, HttpResponse))
+ 
+    def test_status_code(self):
+        response = self.client.get(self.url)
+ 
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_context(self):
+        """Tests the context contains queryset of tasks"""
+        response = self.client.get(self.url)
+ 
+        self.assertIn("tasks", response.context)
+ 
+        tasks = response.context["tasks"]
+ 
+        self.assertTrue(isinstance(tasks, QuerySet))
+        self.assertEqual(tasks.first(), self.task)
+ 
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "temp_index.html")
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from todo.models import Task
+from todo.forms import TaskForm
 
-# Create your views here.
+ 
+ 
+def index(request: HttpRequest) -> HttpResponse:
+ 
+    if request.method == "POST":
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+ 
+    form = TaskForm()
+    tasks = Task.objects.all().order_by("-created")
+ 
+    context = {"tasks": tasks, "form": form, "Status":Task.StatusChoice}
+ 
+    return render(request, "index.html", context)
+
+def update_task_status(
+    request: HttpRequest, task_id: int, new_status: str
+) -> HttpResponse:
+ 
+    if not new_status in Task.StatusChoice.values:
+        return HttpResponseBadRequest("Invalid status")
+ 
+    task = get_object_or_404(Task, id=task_id)
+ 
+    task.status = new_status
+    task.save()
+ 
+    success_url = reverse("index")
+ 
+    return HttpResponseRedirect(success_url)
